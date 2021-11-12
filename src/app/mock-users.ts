d378d3821e2749d0e353086abf307b69d40d823e
Xristo19 <xristochris19@gmail.com>
2021-11-12 22:03:15 +0400

    first commit

---
+++ src/app/mock-users.ts
@@ -0,0 +1,52 @@
+import { List } from './user.interface';
+
+export const STUDENTS: List[] = [{
+  id : 1,
+  studentCode : 19200201,
+  firstName : 'ნიკა',
+  lastName : 'ასაბაშვილი',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+},{
+  id : 2,
+  studentCode : 19200202,
+  firstName : 'ანა',
+  lastName : 'ბიბილაშვილი',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+},{
+  id: 3,
+  studentCode : 19200203,
+  firstName : 'ირენა',
+  lastName : 'გრიგორიანი',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+}, {
+  id: 4,
+  studentCode : 19200147,
+  firstName : 'ლუკა',
+  lastName : 'ირემაშვილი',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+},{
+  id: 5,
+  studentCode : 19200207,
+  firstName : 'ხრისტო',
+  lastName : 'ნაჭყებია',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+}, {
+  id: 6,
+  studentCode : 19200162,
+  firstName : 'პოლინა',
+  lastName : 'ფილიპია',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+}, {
+  id: 7,
+  studentCode : 19200208,
+  firstName : 'გიგი',
+  lastName : 'ჭალიძე',
+  subjects : 'Front End',
+  passDate : new Date('30/12/2020')
+}];
